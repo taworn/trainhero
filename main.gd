8e6aa56366b86c407148582bfa371c6447f1f20b
Taworn T <taworn@gmail.com>
2016-10-26 10:49:50 +0700

    - add title text - add start menu

---
+++ main.gd
@@ -0,0 +1,23 @@
+
+extends Panel
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	var list = get_node("ItemList");
+	list.add_item("New Game", null);
+	list.add_item("Save Data #1", null);
+	list.add_item("Save Data #2", null);
+	list.add_item("Save Data #3", null);
+	list.select(0);
+	list.grab_focus();
+
+func _on_ItemList_item_activated( index ):
+	var list = get_node("ItemList");
+	list.release_focus();
+	list.hide();
+
