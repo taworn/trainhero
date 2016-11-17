255391ab7aad88fa89b05a5945845dba0b4eb2b0
Taworn T <taworn@gmail.com>
2016-11-17 12:34:24 +0700

    - update menu - update status to display character's poison and faint

--- party/status.gd
+++ party/status.gd
@@ -13,14 +13,25 @@ func _ready():
 	mp = get_node("Container/MP")
 
 func update(index):
+	var unusual = false
 	var player = state.persist.players[index]
 	if player.avail:
 		if player.faint:
 			icon.set_animation("faint")
+			unusual = true
 		elif player.poison:
 			icon.set_animation("wound")
+			unusual = true
 		else:
 			icon.set_animation("down")
+		if !unusual:
+			name.set("custom_colors/font_color", Color(1, 1, 1))
+			hp.set("custom_colors/font_color", Color(1, 1, 1))
+			mp.set("custom_colors/font_color", Color(1, 1, 1))
+		else:
+			name.set("custom_colors/font_color", Color(1, 0.2, 0.2))
+			hp.set("custom_colors/font_color", Color(1, 0.2, 0.2))
+			mp.set("custom_colors/font_color", Color(1, 0.2, 0.2))
 		name.set_text(player.name)
 		hp.set_text("HP %d/%d" % [player.hp, player.hp_max])
 		mp.set_text("MP %d/%d" % [player.mp, player.mp_max])
